275b12574c03af2986be82a95185019c3150bf13
JolTheGreat <katoshin0805@gmail.com>
2021-11-29 20:45:04 +0000

    Bug fix

--- src/main/java/com/github/jolthegreat/plugintester/Listener.java
+++ src/main/java/com/github/jolthegreat/plugintester/Listener.java
@@ -32,93 +32,6 @@ public class Listener implements org.bukkit.event.Listener {
     static Inventory inventory = Bukkit.createInventory(null, 54, ChatColor.GOLD + "Slot Machine");
     static boolean lockStart = false;
 
-    public static BiFunction<Player, String, AnvilGUI.Response> getSlotFunction() {
-        return (player, s) -> {
-            Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
-            if (pattern.matcher(s).matches()) {
-                final int bet = Integer.parseInt(s);
-                final List<ItemStack> possible = CasinoContainer.getAll();
-                if (possible.size() == 9) {
-
-                    final ItemStack around = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
-                    ItemMeta aroundMeta = around.getItemMeta();
-                    assert aroundMeta != null;
-                    aroundMeta.setDisplayName(ChatColor.RESET + "");
-                    final int[] aroundPattern = {0, 1, 2, 3, 4, 9, 13, 18, 22, 27, 31, 36, 37, 38, 39, 40};
-                    for (int i : aroundPattern) {
-                        inventory.setItem(i, around);
-                    }
-
-                    final int[] possiblePattern = {10, 11, 12, 19, 20, 21, 28, 29, 30};
-                    for (int i = 0; i < possible.size(); i++) {
-                        inventory.setItem(possiblePattern[i], possible.get(i));
-                    }
-
-                    final ItemStack startButton = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
-                    final ItemMeta startButtonMeta = startButton.getItemMeta();
-                    assert startButtonMeta != null;
-                    startButtonMeta.setDisplayName(ChatColor.GREEN + "Start");
-                    startButton.setItemMeta(startButtonMeta);
-                    inventory.setItem(24, startButton);
-
-                    final ItemStack stopAllButton = new ItemStack(Material.RED_STAINED_GLASS_PANE);
-                    final ItemMeta stopAllButtonMeta = startButton.getItemMeta();
-                    assert stopAllButtonMeta != null;
-                    stopAllButtonMeta.setDisplayName(ChatColor.RED + "Stop");
-                    stopAllButton.setItemMeta(startButtonMeta);
-                    inventory.setItem(25, stopAllButton);
-
-                    final int[] stopButtonPattern = {46, 47, 48};
-
-                    for (int j : stopButtonPattern) {
-                        ItemStack stopButton = new ItemStack(Material.WARPED_BUTTON);
-                        ItemMeta stopButtonMeta = stopButton.getItemMeta();
-                        assert stopButtonMeta != null;
-                        stopButtonMeta.setDisplayName(ChatColor.RED + "Stop slot " + (j - 45));
-                        stopButton.setItemMeta(stopButtonMeta);
-                        inventory.setItem(j, stopButton);
-                    }
-                    player.openInventory(inventory);
-
-                    /**@// TODO: 28/11/2021
-                     * Encapsulate the listeners so there are no duplicates
-                     */
-                    listeners.addSlotFinishListener(() -> {
-                        System.out.println("All stopped");
-                        lockStart = true;
-                    });
-
-
-                    listeners.addButtonPressListener((slotType) -> {
-                        switch (slotType) {
-                            case ONE -> {
-                                tasks.get(0).cancel();
-                            }
-                            case TWO -> {
-                                tasks.get(1).cancel();
-                            }
-                            case THREE -> {
-                                tasks.get(2).cancel();
-                            }
-                            case ALL -> {
-                                tasks.forEach(BukkitTask::cancel);
-                            }
-                        }
-
-                        if (tasks.get(0).isCancelled() && tasks.get(1).isCancelled() && tasks.get(2).isCancelled())
-                            listeners.slotFinishTrigger();
-                    });
-
-                } else {
-                    System.out.println(Color.RED + "you can only have 9 items in casino");
-                }
-            } else {
-                player.sendMessage(ChatColor.RED + "Please enter proper number as an input");
-            }
-            return AnvilGUI.Response.close();
-        };
-    }
-
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         System.out.println("inventory close");
@@ -180,16 +93,99 @@ public class Listener implements org.bukkit.event.Listener {
                             System.out.println("Cycle3");
                         }, 0, 5));
                     }
-
                 }
-                case RED_STAINED_GLASS_PANE -> {
-                    tasks.forEach(BukkitTask::cancel);
-                    listeners.slotFinishTrigger();
-                }
-
             }
             event.setCancelled(true);
         }
+    }
+
+    public static BiFunction<Player, String, AnvilGUI.Response> getSlotFunction() {
+        return (player, s) -> {
+            listeners.clearButtonPressListeners();
+            listeners.clearSlotFinishListeners();
+            tasks.clear();
+            Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
+            if (pattern.matcher(s).matches()) {
+                final int bet = Integer.parseInt(s);
+                final List<ItemStack> possible = CasinoContainer.getAll();
+                if (possible.size() == 9) {
+                    final ItemStack around = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
+                    ItemMeta aroundMeta = around.getItemMeta();
+                    assert aroundMeta != null;
+                    aroundMeta.setDisplayName(ChatColor.RESET + "");
+                    final int[] aroundPattern = {0, 1, 2, 3, 4, 9, 13, 18, 22, 27, 31, 36, 37, 38, 39, 40};
+                    for (int i : aroundPattern) {
+                        inventory.setItem(i, around);
+                    }
+
+                    final int[] possiblePattern = {10, 11, 12, 19, 20, 21, 28, 29, 30};
+                    for (int i = 0; i < possible.size(); i++) {
+                        inventory.setItem(possiblePattern[i], possible.get(i));
+                    }
+
+                    final ItemStack startButton = new ItemStack(Material.GREEN_STAINED_GLASS_PANE);
+                    final ItemMeta startButtonMeta = startButton.getItemMeta();
+                    assert startButtonMeta != null;
+                    startButtonMeta.setDisplayName(ChatColor.GREEN + "Start");
+                    startButton.setItemMeta(startButtonMeta);
+                    inventory.setItem(24, startButton);
+
+                    final int[] stopButtonPattern = {46, 47, 48};
+
+                    for (int j : stopButtonPattern) {
+                        ItemStack stopButton = new ItemStack(Material.WARPED_BUTTON);
+                        ItemMeta stopButtonMeta = stopButton.getItemMeta();
+                        assert stopButtonMeta != null;
+                        stopButtonMeta.setDisplayName(ChatColor.RED + "Stop slot " + (j - 45));
+                        stopButton.setItemMeta(stopButtonMeta);
+                        inventory.setItem(j, stopButton);
+                    }
+                    player.openInventory(inventory);
+
+                    listeners.addSlotFinishListener(() -> {
+                        System.out.println("All stopped");
+                        lockStart = true;
+                    });
+
+
+
+                    listeners.addButtonPressListener((slotType) -> {
+                        tasks.forEach((bukkitTask) -> {
+                            System.out.println(bukkitTask.isCancelled());
+                        });
+                        switch (slotType) {
+                            case ONE -> {
+                                tasks.get(0).cancel();
+                                System.out.println("Canceled first task");
+                            }
+                            case TWO -> {
+                                tasks.get(1).cancel();
+                                System.out.println("Canceled second task");
+                            }
+                            case THREE -> {
+                                tasks.get(2).cancel();
+                                System.out.println("Canceled third task");
+                            }
+                            case ALL -> {
+                                tasks.forEach(BukkitTask::cancel);
+                            }
+                        }
+
+                        if (tasks.get(0).isCancelled() && tasks.get(1).isCancelled() && tasks.get(2).isCancelled()) {
+                            System.out.println("Triggering slot finish");
+                            listeners.slotFinishTrigger();
+                        }
+
+                    });
+
+                } else {
+                    System.out.println(Color.RED + "you can only have 9 items in casino");
+                }
+            } else {
+                player.sendMessage(ChatColor.RED + "Please enter proper number as an input");
+            }
+            return AnvilGUI.Response.close();
+        };
     }
 
     public SlotType intToSlotType(int i) {
@@ -212,8 +208,8 @@ public class Listener implements org.bukkit.event.Listener {
 }
 
 class SlotListeners {
-    private List<Consumer<SlotType>> buttonPressListeners = new ArrayList<>();
-    private List<Runnable> slotFinishListener = new ArrayList<>();
+    private final List<Consumer<SlotType>> buttonPressListeners = new ArrayList<>();
+    private final List<Runnable> slotFinishListeners = new ArrayList<>();
 
     public void addButtonPressListener(Consumer<SlotType> press) {
         buttonPressListeners.add(press);
@@ -224,11 +220,19 @@ class SlotListeners {
     }
 
     public void addSlotFinishListener(Runnable runnable) {
-        slotFinishListener.add(runnable);
+        slotFinishListeners.add(runnable);
     }
 
     public void slotFinishTrigger() {
-        slotFinishListener.forEach(Runnable::run);
+        slotFinishListeners.forEach(Runnable::run);
+    }
+
+    public void clearButtonPressListeners() {
+        buttonPressListeners.clear();
+    }
+
+    public void clearSlotFinishListeners() {
+        slotFinishListeners.clear();
     }
 }
 
