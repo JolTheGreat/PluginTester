b39f810ff4427b57ab7d8e2fc544f1ddf2cb8fb4
JolTheGreat <katoshin0805@gmail.com>
2021-11-29 18:17:45 +0000

    Encapsulated the Listener

--- src/main/java/com/github/jolthegreat/plugintester/Listener.java
+++ src/main/java/com/github/jolthegreat/plugintester/Listener.java
@@ -9,7 +9,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -29,21 +28,17 @@ enum SlotType {
 
 public class Listener implements org.bukkit.event.Listener {
     final private static ArrayList<BukkitTask> tasks = new ArrayList<>();
-
-
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        System.out.println("Joined");
-    }
+    final static SlotListeners listeners = new SlotListeners();
+    static Inventory inventory = Bukkit.createInventory(null, 54, ChatColor.GOLD + "Slot Machine");
+    static boolean lockStart = false;
 
     public static BiFunction<Player, String, AnvilGUI.Response> getSlotFunction() {
         return (player, s) -> {
-            System.out.println(s);
             Pattern pattern = Pattern.compile("-?\\d+(\\.\\d+)?");
             if (pattern.matcher(s).matches()) {
+                final int bet = Integer.parseInt(s);
                 final List<ItemStack> possible = CasinoContainer.getAll();
                 if (possible.size() == 9) {
-                    Inventory inventory = Bukkit.createInventory(player, 54, ChatColor.GOLD + "Slot Machine");
 
                     final ItemStack around = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
                     ItemMeta aroundMeta = around.getItemMeta();
@@ -66,6 +61,12 @@ public class Listener implements org.bukkit.event.Listener {
                     startButton.setItemMeta(startButtonMeta);
                     inventory.setItem(24, startButton);
 
+                    final ItemStack stopAllButton = new ItemStack(Material.RED_STAINED_GLASS_PANE);
+                    final ItemMeta stopAllButtonMeta = startButton.getItemMeta();
+                    assert stopAllButtonMeta != null;
+                    stopAllButtonMeta.setDisplayName(ChatColor.RED + "Stop");
+                    stopAllButton.setItemMeta(startButtonMeta);
+                    inventory.setItem(25, stopAllButton);
 
                     final int[] stopButtonPattern = {46, 47, 48};
 
@@ -79,39 +80,33 @@ public class Listener implements org.bukkit.event.Listener {
                     }
                     player.openInventory(inventory);
 
-
-                    tasks.add(Bukkit.getScheduler().runTaskTimer(PluginTester.INSTANCE, () -> {
-                        ItemStack newSlot = CasinoContainer.getRandom();
-                        inventory.setItem(28, inventory.getItem(19));
-                        inventory.setItem(19, inventory.getItem(10));
-                        inventory.setItem(10, newSlot);
-                        System.out.println("Cycle1");
-                    }, 0, 5));
-
-                    tasks.add(Bukkit.getScheduler().runTaskTimer(PluginTester.INSTANCE, () -> {
-                        ItemStack newSlot = CasinoContainer.getRandom();
-                        inventory.setItem(29, inventory.getItem(20));
-                        inventory.setItem(20, inventory.getItem(11));
-                        inventory.setItem(11, newSlot);
-                        System.out.println("Cycle2");
-                    }, 0, 5));
-                    tasks.add(Bukkit.getScheduler().runTaskTimer(PluginTester.INSTANCE, () -> {
-                        ItemStack newSlot = CasinoContainer.getRandom();
-                        inventory.setItem(30, inventory.getItem(21));
-                        inventory.setItem(21, inventory.getItem(12));
-                        inventory.setItem(12, newSlot);
-                        System.out.println("Cycle3");
-                    }, 0, 5));
+                    /**@// TODO: 28/11/2021
+                     * Encapsulate the listeners so there are no duplicates
+                     */
+                    listeners.addSlotFinishListener(() -> {
+                        System.out.println("All stopped");
+                        lockStart = true;
+                    });
 
 
-                    ButtonPress.addListener((slotType) -> {
+                    listeners.addButtonPressListener((slotType) -> {
                         switch (slotType) {
-                            case ONE -> tasks.get(0).cancel();
-                            case TWO -> tasks.get(1).cancel();
-                            case THREE -> tasks.get(2).cancel();
-                            case ALL -> tasks.forEach(BukkitTask::cancel);
-                            default -> System.out.println("Pls select proper stuff");
+                            case ONE -> {
+                                tasks.get(0).cancel();
+                            }
+                            case TWO -> {
+                                tasks.get(1).cancel();
+                            }
+                            case THREE -> {
+                                tasks.get(2).cancel();
+                            }
+                            case ALL -> {
+                                tasks.forEach(BukkitTask::cancel);
+                            }
                         }
+
+                        if (tasks.get(0).isCancelled() && tasks.get(1).isCancelled() && tasks.get(2).isCancelled())
+                            listeners.slotFinishTrigger();
                     });
 
                 } else {
@@ -127,33 +122,17 @@ public class Listener implements org.bukkit.event.Listener {
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event) {
         System.out.println("inventory close");
+        lockStart = false;
         if (event.getView().getTitle().equalsIgnoreCase("Test") || event.getView().getTitle().equalsIgnoreCase(ChatColor.GOLD + "Slot Machine")) {
             System.out.println("Attempting the interrupt");
-            ButtonPress.buttonPressTrigger(SlotType.ALL);
-
-        }
-    }
-
-    public SlotType intToSlotType(int i) {
-        switch (i) {
-            case 1 -> {
-                return SlotType.ONE;
-            }
-            case 2 -> {
-                return SlotType.TWO;
-            }
-            case 3 -> {
-                return SlotType.THREE;
-            }
-            default -> {
-                return SlotType.ALL;
-            }
+            listeners.buttonPressTrigger(SlotType.ALL);
         }
     }
 
     @EventHandler
     public void inventoryClick(InventoryClickEvent event) {
         ItemStack eventStack = event.getCurrentItem();
+
         Player player = (Player) event.getWhoClicked();
         if (event.getView().getTitle().equalsIgnoreCase("Test") || event.getView().getTitle().equalsIgnoreCase(ChatColor.GOLD + "Slot Machine")) {
             switch (Objects.requireNonNull(eventStack).getType()) {
@@ -173,24 +152,83 @@ public class Listener implements org.bukkit.event.Listener {
                     final String displayName = Objects.requireNonNull(Objects.requireNonNull(event.getCurrentItem()).getItemMeta()).getDisplayName();
                     if (displayName.contains("Stop slot")) {
                         System.out.println("Stop");
-                        ButtonPress.buttonPressTrigger(intToSlotType(Integer.parseInt(displayName.split(" ")[2])));
+                        listeners.buttonPressTrigger(intToSlotType(Integer.parseInt(displayName.split(" ")[2])));
                     }
                 }
+                case GREEN_STAINED_GLASS_PANE -> {
+                    if (!lockStart) {
+                        tasks.add(Bukkit.getScheduler().runTaskTimer(PluginTester.INSTANCE, () -> {
+                            ItemStack newSlot = CasinoContainer.getRandom();
+                            inventory.setItem(28, inventory.getItem(19));
+                            inventory.setItem(19, inventory.getItem(10));
+                            inventory.setItem(10, newSlot);
+                            System.out.println("Cycle1");
+                        }, 0, 5));
+
+                        tasks.add(Bukkit.getScheduler().runTaskTimer(PluginTester.INSTANCE, () -> {
+                            ItemStack newSlot = CasinoContainer.getRandom();
+                            inventory.setItem(29, inventory.getItem(20));
+                            inventory.setItem(20, inventory.getItem(11));
+                            inventory.setItem(11, newSlot);
+                            System.out.println("Cycle2");
+                        }, 0, 5));
+                        tasks.add(Bukkit.getScheduler().runTaskTimer(PluginTester.INSTANCE, () -> {
+                            ItemStack newSlot = CasinoContainer.getRandom();
+                            inventory.setItem(30, inventory.getItem(21));
+                            inventory.setItem(21, inventory.getItem(12));
+                            inventory.setItem(12, newSlot);
+                            System.out.println("Cycle3");
+                        }, 0, 5));
+                    }
+
+                }
+                case RED_STAINED_GLASS_PANE -> {
+                    tasks.forEach(BukkitTask::cancel);
+                    listeners.slotFinishTrigger();
+                }
+
             }
             event.setCancelled(true);
         }
     }
+
+    public SlotType intToSlotType(int i) {
+        switch (i) {
+            case 1 -> {
+                return SlotType.ONE;
+            }
+            case 2 -> {
+                return SlotType.TWO;
+            }
+            case 3 -> {
+                return SlotType.THREE;
+            }
+            default -> {
+                return SlotType.ALL;
+            }
+
+        }
+    }
 }
 
-class ButtonPress {
-    static List<Consumer<SlotType>> listeners = new ArrayList<>();
+class SlotListeners {
+    private List<Consumer<SlotType>> buttonPressListeners = new ArrayList<>();
+    private List<Runnable> slotFinishListener = new ArrayList<>();
 
-    public static void addListener(Consumer<SlotType> press) {
-        System.out.println(press);
-        listeners.add(press);
+    public void addButtonPressListener(Consumer<SlotType> press) {
+        buttonPressListeners.add(press);
     }
 
-    public static void buttonPressTrigger(SlotType slotType) {
-        listeners.forEach((listener) -> listener.accept(slotType));
+    public void buttonPressTrigger(SlotType slotType) {
+        buttonPressListeners.forEach((listener) -> listener.accept(slotType));
+    }
+
+    public void addSlotFinishListener(Runnable runnable) {
+        slotFinishListener.add(runnable);
+    }
+
+    public void slotFinishTrigger() {
+        slotFinishListener.forEach(Runnable::run);
     }
 }
+
